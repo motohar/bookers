96068160f9171e8170772ff2eab098ab391e7729
EC2 Default User <110407718+motohar@users.noreply.github.com>
2022-08-31 11:31:34 +0000

    [Add]bookers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "a805aa10271f46cdb6c93c4f06fca4fe.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
